c9b37e64a60389fbb93fb0b795d3fcd5b0cf8136
Joel Pereira Santander <joel.pereira@viggo.cl>
2024-05-20 10:28:11 -0400

    query inner

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+select idusuario,idrecinto,nombrerecinto from app.Recinto where idusuario = 6
+
+
+
+select nombrecompleto from per.dotacion where NombreCompleto like '%j%'
+
+
+select * from per.Dotacion where IdEmpleado=167988 or IdEmpleado=39453;
