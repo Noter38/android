08ed97ebd15535a7a92b3060dabfa919a309dec7
Joel Pereira Santander <joel.pereira@viggo.cl>
2024-05-20 10:34:51 -0400

    comentario

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,3 +6,7 @@ select nombrecompleto from per.dotacion where NombreCompleto like '%j%'
 
 
 select * from per.Dotacion where IdEmpleado=167988 or IdEmpleado=39453;
+
+//aca aprendiendo con crsitobal 
+
+
